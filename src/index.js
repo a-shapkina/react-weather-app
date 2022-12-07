599d5ce9039460a7bd06ab83ef45c6b5160ae900
Alexandra S <alexandra04091981@hotmail.com>
2022-12-07 07:41:11 +0100

    Added weather search

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import WeatherSearch from "./WeatherSearch";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div className="App">
+    <h1>Weather App</h1>
+    <WeatherSearch />
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
