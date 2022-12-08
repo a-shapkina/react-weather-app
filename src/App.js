7e31f37f38591063e80e3ff0abbd504b98afd964
Alexandra S <alexandra04091981@hotmail.com>
2022-12-08 08:58:56 +0100

    Added HTML structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from "react";
 import "./App.css";
-import WeatherSearch from "./WeatherSearch";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
-      <WeatherSearch />
-      <footer>
-        <small>
-          <a
-            href="https://github.com/a-shapkina/react-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GitHub
-          </a>{" "}
-          © 2022 <a href="https://ashapkina.com">Alexandra Shapkina</a>
-        </small>
-      </footer>
+      <div className="container">
+        <Weather />
+
+        <footer>
+          <small>
+            <a
+              href="https://github.com/a-shapkina/react-weather-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>{" "}
+            © 2022 <a href="https://ashapkina.com">Alexandra Shapkina</a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
