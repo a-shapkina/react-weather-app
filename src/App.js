07c2ac15a12de1df26788fcfd6309f296101e54f
Alexandra S <alexandra04091981@hotmail.com>
2022-12-08 07:28:17 +0100

    Moved code from index.js to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import WeatherSearch from "./WeatherSearch";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>React Weather App</h1>
+      <WeatherSearch />
+      <footer>
+        <small>
+          <a
+            href="https://github.com/a-shapkina/react-weather-app"
+            target="_blank"
+          >
+            GitHub
+          </a>{" "}
+          © 2022 <a href="https://ashapkina.com">Alexandra Shapkina</a>
+        </small>
+      </footer>
+    </div>
+  );
 }
