eb4683c3d7ffd12ca3aaa42ab1760c9093f4573e
Alexandra S <alexandra04091981@hotmail.com>
2022-12-09 14:40:27 +0100

    Changed apiKey (looping issue)

--- src/Weather.js
+++ src/Weather.js
@@ -68,7 +68,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    const apiKey = "5od0e7351f458867cdf2ab24f80tba46";
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
     return "Loading...";
