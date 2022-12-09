45e3a50dc76ede285a1485f09dc1d387acc44617
Alexandra S <alexandra04091981@hotmail.com>
2022-12-09 14:32:55 +0100

    Revert "Fixed looping issue"
    This reverts commit 3c01aaed2e66d7b6d8291183d3a21950bd4dccfe.

--- src/Weather.js
+++ src/Weather.js
@@ -1,22 +1,21 @@
 import React, { useState } from "react";
-import axios from "axios";
 import "./Weather.css";
+import axios from "axios";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      date: "Wednesday 07:00",
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
-      date: "Wednesday 07:00",
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
       wind: response.data.wind.speed,
       city: response.data.name,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png",
     });
   }
-
   if (weatherData.ready) {
     return (
       <div className="Weather">
@@ -25,9 +24,8 @@ export default function Weather(props) {
             <div className="col-9">
               <input
                 type="search"
-                placeholder="Enter a city.."
+                placeHolder="Enter a city"
                 className="form-control"
-                autoFocus="on"
               />
             </div>
             <div className="col-3">
@@ -46,13 +44,12 @@ export default function Weather(props) {
         </ul>
         <div className="row mt-3">
           <div className="col-6">
-            <div className="clearfix">
-              <img
-                src={weatherData.iconUrl}
-                alt={weatherData.description}
-                className="float-left"
-              />
-              <div className="float-left">
+            <div className="d-flex">
+              <div>
+                <img src={weatherData.iconUrl} alt={weatherData.description} />
+              </div>
+
+              <div>
                 <span className="temperature">
                   {Math.round(weatherData.temperature)}
                 </span>
@@ -62,7 +59,8 @@ export default function Weather(props) {
           </div>
           <div className="col-6">
             <ul>
-              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Precipitation: 15%</li>
+              <li>Humidily: {weatherData.humidity}%</li>
               <li>Wind: {weatherData.wind} km/h</li>
             </ul>
           </div>
@@ -70,10 +68,9 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    const apiKey = "5od0e7351f458867cdf2ab24f80tba46";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
-
     return "Loading...";
   }
 }
