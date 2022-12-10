eecbb25cf41d91e7736a5eec81ccb0a16a95eaaa
Alexandra S <alexandra04091981@hotmail.com>
2022-12-10 11:43:55 +0100

    Added custom animated weather icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./Formatted";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -16,7 +17,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="d-flex">
             <div>
-              <img src={props.data.iconUrl} alt={props.data.description} />
+              <WeatherIcon code={props.data.icon} size={52} />
             </div>
 
             <div>
